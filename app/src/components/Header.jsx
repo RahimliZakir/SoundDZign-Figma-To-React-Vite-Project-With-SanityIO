cf787ab21679eee0544890afd2447b2fe95df410
RahimliZakir <zakirer@code.edu.az>
2023-09-06 02:59:27 +0400

    Done for yet

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -5,9 +5,11 @@ const Header = () => {
     <section
       id="header"
       style={{ "--header-banner": `url(${HeaderBanner})` }}
-      className={`bg-[image:var(--header-banner)]`}
+      className={`bg-[image:var(--header-banner)] h-screen w-[1280px] container`}
     >
-      <div className="container relative h-screen w-[1280px]"></div>
+      <div className="relative h-screen">
+        <div className="w-[455px] h-[330px] absolute rounded-t-[20px] bg-black/50 backdrop-blur-[25px] border-white border-[4px] border-b-0 right-0 bottom-0"></div>
+      </div>
     </section>
   );
 };
--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -1,29 +1,33 @@
 const Nav = () => {
   return (
-    <nav className="fixed flex items-center p-8 z-[999] top-6 left-[10%] right-[10%] h-[80px] bg-black/50 rounded-bl-[10px] rounded-br-[10px] backdrop-blur-[20px]">
-      <div className="w-1/2">
-        <h6 className="font-inter text-white font-extrabold">
-          Soun<span className="text-red">DZign</span>
-        </h6>
-      </div>
-      <div className="right-nav w-1/2">
-        <ul
-          style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
-          className="flex justify-between text-white font-extrabold font-inter"
-        >
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Course Details</a>
-          </li>
-          <li>
-            <a href="#">Blog</a>
-          </li>
-          <li>
-            <a href="#">Testimonials</a>
-          </li>
-        </ul>
+    <nav className="fixed z-[999] top-6 w-full">
+      <div className="container">
+        <div className="flex flex-wrap items-center px-10 h-[80px] bg-black/50 rounded-b-[10px] backdrop-blur-[20px]">
+          <div className="w-1/2">
+            <h6 className="font-inter text-white font-extrabold">
+              Soun<span className="text-red">DZign</span>
+            </h6>
+          </div>
+          <div className="right-nav w-1/2">
+            <ul
+              style={{ textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)" }}
+              className="flex justify-between text-white font-extrabold font-inter"
+            >
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Course Details</a>
+              </li>
+              <li>
+                <a href="#">Blog</a>
+              </li>
+              <li>
+                <a href="#">Testimonials</a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
